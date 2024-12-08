80c060153baae529d2291ee23a7e0baef41be0cb
macktsawyer <macktsawyer@yahoo.com>
2024-12-08 17:55:43 -0500

    Adding a new project, which required a preview feature added.

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ let observer = new IntersectionObserver(inViewCallback, options);
 observedElements.forEach(element => {
     observer.observe(element);
 })
+
+document.querySelector('.previewButton').addEventListener('click', () => {
+    const image = document.getElementById('previewContainer');
+    image.style.display = image.style.display === 'none' ? 'inline-block' : 'none';
+  });
+
+document.querySelector('#previewContainer').addEventListener('click', () => {
+    const image = document.getElementById('previewContainer');
+    image.style.display = image.style.display === 'none' ? 'inline-block' : 'none';
+});
